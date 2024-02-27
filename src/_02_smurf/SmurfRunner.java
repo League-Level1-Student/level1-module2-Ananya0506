a66fa7fb0008a6dcbccbf500d0db042b1ae84bf5
league <league@192.168.1.72>
2024-02-26 16:55:50 -0800

    am on smurf

---
+++ src/_02_smurf/SmurfRunner.java
@@ -0,0 +1,13 @@
+package _02_smurf;
+
+public class SmurfRunner {
+
+	public static void main(String[] args) {
+		Smurf Handy = new Smurf("Handy");
+		Handy.eat();
+		Handy.getName();
+		
+
+	}
+
+}
